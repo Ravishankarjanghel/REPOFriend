6dca5fd1dd1ec51a65b5747350867c82504bd003
Ravishankarjanghel <ravijanghel@gmail.com>
2023-06-01 14:08:57 +0530

    1st commit.

---
+++ src/test/java/Sample01.java
@@ -0,0 +1,12 @@
+
+public class Sample01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//thank you
+		//welcome thank you 
+		System.out.println("test case successfully completed");
+
+	}
+
+}
